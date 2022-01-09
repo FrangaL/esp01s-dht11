e9e3d983209f33ba933d68eb1118fb21a128b419
Fran <frangal@gmail.com>
2022-01-09 19:19:52 +0100

    Calibrate temp

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def read_sensor():
     hum = sensor.humidity()
     if (isinstance(temp, float) and isinstance(hum, float)) or (isinstance(temp, int) and isinstance(hum, int)):
       msg = (b'{0:3.1f},{1:3.1f}'.format(temp, hum))
-      # uncomment for Fahrenheit
-      #temp = temp * (9/5) + 32.0
+      # temperature to Fahrenheit: temp = temp * (9/5) + 32.0
+      # calibrate temperature
+      temp = temp - 4.0
       hum = round(hum, 2)
       return(msg)
     else:
